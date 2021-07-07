87715f920757bcf275a15eb3907f3baec43f7db6
79remat <59704956+79remat@users.noreply.github.com>
2021-07-07 02:07:02 -0400

    Troubleshooting Navbar Auto Close on Mobile

--- js/custom.js
+++ js/custom.js
@@ -35,6 +35,11 @@
 		}
 	});
 	
+	// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
 	/* ..............................................
     Gallery
     ................................................. */
